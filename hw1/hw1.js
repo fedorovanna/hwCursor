ee2a92e5cfd06729ae163b18354519ac22e62345
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-02-22 12:37:36 +0200

    hw1

---
+++ hw1/hw1.js
@@ -0,0 +1,18 @@
+const productPrice1 = 15.678
+const productPrice2 = 123.9655
+const productPrice3 = 90.2345
+console.log('Max:', Math.max(productPrice1, productPrice2, productPrice3))
+console.log('Min:', Math.min(productPrice1, productPrice2, productPrice3))
+totalPrice = productPrice1 + productPrice2 + productPrice3
+console.log('Total price:', totalPrice)
+sum = Math.floor(productPrice1) + Math.floor(productPrice2) + Math.floor(productPrice3)
+console.log(`Total price rounded to hundreds: ${Math.round(sum/100)*100}`)
+const evenOrOdd = (Math.floor(totalPrice) % 2  == 0) ? true : false;
+console.log('Even:', evenOrOdd)
+console.log('Change for 500 uah: ', 500 - totalPrice)   
+console.log('Average:', ((productPrice1+productPrice2+productPrice3)/3).toFixed(2))
+randomDiscount=Math.random()*100
+discountPrice=totalPrice-((totalPrice*randomDiscount)/100)
+console.log('Price with discount:', discountPrice.toFixed(2))
+netProfit=discountPrice-totalPrice/2
+console.log('Net profit:', netProfit)
