adc6d6a1aabd43bacc6ba605e85c9b6725408caf
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-02-22 23:07:45 +0200

    hw1

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -5,8 +5,8 @@ console.log('Max:', Math.max(productPrice1, productPrice2, productPrice3))
 console.log('Min:', Math.min(productPrice1, productPrice2, productPrice3))
 totalPrice = productPrice1 + productPrice2 + productPrice3
 console.log('Total price:', totalPrice)
-sum = Math.floor(productPrice1) + Math.floor(productPrice2) + Math.floor(productPrice3)
-console.log(`Total price rounded to hundreds: ${Math.round(sum/100)*100}`)
+roundedTotalPrice = Math.floor(productPrice1) + Math.floor(productPrice2) + Math.floor(productPrice3)
+console.log(`Total price rounded to hundreds: ${Math.round(roundedTotalPrice/100)*100}`)
 const evenOrOdd = (Math.floor(totalPrice) % 2  == 0) ? true : false;
 console.log('Even:', evenOrOdd)
 console.log('Change for 500 uah: ', (500 - totalPrice).toFixed(2))   
