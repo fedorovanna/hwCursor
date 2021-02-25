b33a6011112343ce04315d3218470c70ab137eed
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-02-25 14:45:16 +0200

    hw1

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,21 +1,24 @@
 const productPrice1 = 15.678
 const productPrice2 = 123.9655
 const productPrice3 = 90.2345
-console.log('Max:', Math.max(productPrice1, productPrice2, productPrice3))
-console.log('Min:', Math.min(productPrice1, productPrice2, productPrice3))
-totalPrice = productPrice1 + productPrice2 + productPrice3
-console.log('Total price:', totalPrice)
+const maxPrice = Math.max(productPrice1, productPrice2, productPrice3)
+console.log('Max:', maxPrice)
+const minPrice = Math.min(productPrice1, productPrice2, productPrice3)
+console.log('Min:', minPrice)
+const totalPrice = productPrice1 + productPrice2 + productPrice3
+console.log('Total price:', totalPrice.toFixed(2))
 roundedTotalPrice = Math.floor(productPrice1) + Math.floor(productPrice2) + Math.floor(productPrice3)
 console.log('Rounded total price:', roundedTotalPrice)
 console.log(`Total price rounded to hundreds: ${Math.round(roundedTotalPrice/100)*100}`)
 const evenOrOdd = (Math.floor(roundedTotalPrice) % 2  == 0) ? true : false;
 console.log('Even:', evenOrOdd)
-console.log('Change for 500 uah: ', 500 - totalPrice)  
+const clientMoney = 500
+console.log('Change for 500 uah: ', (clientMoney - totalPrice).toFixed(2))  
 averagePrice= (productPrice1+productPrice2+productPrice3)/3
 console.log('Average:', averagePrice.toFixed(2))
 randomDiscount=Math.random()*100
 console.log('Discount:', randomDiscount)
 discountPrice=totalPrice-((totalPrice*randomDiscount)/100)
 console.log('Price with discount:', discountPrice.toFixed(2))
-netProfit=discountPrice-totalPrice/2
-console.log('Net profit:', netProfit)
+netProfit=totalPrice/2-(totalPrice-discountPrice)
+console.log('Net profit:', netProfit.toFixed(2))
--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,1 +1,3 @@
-prompt('Enter the first integer number')
+let N = prompt('Enter the first integer number')
+let M = prompt('Enter the last integer number')
+
