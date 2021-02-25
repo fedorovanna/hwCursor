47acf99592be881a8f638dedc0b5b7e8dd28ebe3
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-02-25 22:27:21 +0200

    hw1

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,11 +1,29 @@
-let N
+let N 
 do
 {
  N = parseInt(prompt('Enter the first integer number'))
-};
-while (Number.isNaN(N));
-
-    //alert('Invalid input! Enter the integer number!')
-
-//let M = prompt('Enter the last integer number')
-
+}
+while (N==null||Number.isNaN(N)||N<=0)
+let M
+do
+{
+ M = parseInt(prompt('Enter the last integer number'))
+}
+while (M==null||Number.isNaN(M)||M<=0)
+let skipEven = confirm('Skip even numbers?')
+sumOfNumbers=0
+for (let i = N; i <= M; i++) 
+{
+    if(skipEven)
+    {
+        if(i%2!=0)
+        {
+            sumOfNumbers+=i
+        }
+    }
+    else
+    {
+        sumOfNumbers +=i;
+    }
+}
+console.log('Sum=', sumOfNumbers)
