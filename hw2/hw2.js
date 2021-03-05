65d84cb2ed5e2392ce8fa6b9914dd56e6af91a8e
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-03-05 22:10:15 +0200

    hw4

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,40 +1,28 @@
 let N = 0
-do
-{
+do {
  N = parseInt(prompt('Enter the first integer number'))
-}
-while (N == null || Number.isNaN(N) || N <= 0)
+} while (N == null || Number.isNaN(N) || N <= 0 || Number.isInteger(N) == false)
 let M = 0
-do
-{
+do{
  M = parseInt(prompt('Enter the last integer number'))
-}
-while (M == null || Number.isNaN(M) || M <= 0)
+} while (M == null || Number.isNaN(M) || M <= 0 || Number.isInteger(M) == false)
 let skipEven = confirm('Skip even numbers?')
 sumOfNumbers = 0
-if (M<N)
-{
+if (M<N) {
     let temp = N
     N = M
     M = temp
 }
-if (skipEven)
-{
+if (skipEven) {
     console.log('Skipping even numbers')
-    for (let i = N; i <= M; i++)
-    {
-        if (i % 2 != 0)
-        {
-            {
-                sumOfNumbers += i
-            }
+    for (let i = N; i <= M; i++){
+        if (i % 2 != 0) {
+            sumOfNumbers += i
         }
     }
 }
-else
-{
-    for (let i = N; i <= M; i++)
-    {
+else {
+    for (let i = N; i <= M; i++) {
         sumOfNumbers += i;
     }    
 }
--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,1 +1,52 @@
-getRandomArray(length, min, max)
+function getRandomArray(length, min, max)
+{
+    let randomArray = []
+    for (let i = 0; i < length; i++) 
+    {
+        randomArray[i] = Math.floor(Math.random() * (max - min + 1)) + min        
+    }
+    return randomArray
+}
+
+function getAverage(...numbers)
+{
+    const integerNumbers = numbers.filter(number => Number.isInteger(number))
+    const average = integerNumbers.reduce((total, number) => (total + number), 0)/integerNumbers.length
+    return average
+}
+
+function filterEvenNumbers(...numbers)
+{
+    const oddNumbers = numbers.filter(number => number % 2 != 0).join(', ')
+    return oddNumbers
+}
+
+function countPositiveNumbers(...numbers)
+{
+    const positiveNumbers = numbers.filter(number => number > 0)
+    return positiveNumbers.length
+}
+
+function getDividedByFive(...numbers)
+{
+    const dividedByFive = numbers.filter(number => number % 5 == 0).join(', ')
+    return dividedByFive
+}
+
+function divideByThree(word)
+{
+    const dividedWord = word.forEach((letter, i, arr) => { arr.slice(i, i+2)});
+    return dividedWord
+}
+const randomArray = getRandomArray(5, 1, 14)
+console.log('Random array:', randomArray)
+const oddNumbers = filterEvenNumbers(1, 2, 3, 4)
+console.log('Odd numbers:', oddNumbers)
+const dividedByFive = getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
+console.log('Divided by five:', dividedByFive)
+const average = getAverage(1, 2, 2.3, 5, 8)
+console.log('Average:', average)
+const positiveNumbers = countPositiveNumbers(1, -2, 3, -4, -5, 6)
+console.log('Positive numbers:', positiveNumbers)
+const dividedWord = divideByThree('exceptions')
+console.log(dividedWord)
