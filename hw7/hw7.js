2e4376feb6cc50fca604e5bcec3e02531cef3c2e
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-03-15 20:20:56 +0200

    hw7

---
+++ hw7/hw7.js
@@ -0,0 +1,28 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+function getMyTaxes(salary){
+    return this.tax * salary
+}
+
+function getMiddleTaxes(country){
+    return this.tax * this.middleSalary
+}
+
+function getTotalTaxes(country){
+    return (this.tax * this.middleSalary * this.vacancies)
+}
+
+function getMySalary(country){
+    setInterval(()=>{
+    let obj = {}
+    obj.salary = Math.floor((Math.random()*500)+1500)
+    obj.taxes = this.tax * obj.salary
+    obj.profit = obj.salary - obj.taxes
+    console.log(obj)}, 10000)
+}
+console.log('My tax:', getMyTaxes.call(ukraine, 1700))
+console.log('Middle tax:', getMiddleTaxes.call(litva))
+console.log('Total tax:', getTotalTaxes.call(latvia))
+getMySalary.call(litva)
