f8f963e90af3851a27554ea602db0cd7af1973d1
fedorovanna <63235338+fedorovanna@users.noreply.github.com>
2021-04-21 15:52:05 +0300

    hw15

--- hw15/hw15.js
+++ hw15/hw15.js
@@ -1,8 +1,35 @@
 function* createIdGenerator(){
     let i = 0;
     while(true)
-        yield i++;
+    yield i++;
 }
 
 const idGenerator = createIdGenerator();
+
+function* newFontGenerator(defSize) {
+    const text = document.querySelector('#sample');
+    text.style.fontSize = defSize + 'pt';
+    let size = Number.parseInt(text.style.fontSize);
+    let value = null;
+    while(true){
+        if (value === "up") {
+            size += 2;
+            text.style.fontSize = size + 'pt';
+        } else if (value === "down") {
+            size -= 2;
+            text.style.fontSize = size + 'pt';
+        }
+        value = yield;
+        document.querySelector('.current').textContent = text.style.fontSize
+    }
+    
+}
 const fontGenerator = newFontGenerator(14);
+const downButton = document.querySelector('#down')
+const upButton = document.querySelector('#up')
+downButton.addEventListener('click', ()=>{
+    fontGenerator.next('down').value;
+})
+upButton.addEventListener('click', ()=>{
+    fontGenerator.next('up').value;
+})  
